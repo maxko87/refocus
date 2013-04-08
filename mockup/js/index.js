d529ed0325a732f446931b742f8111f6d18e9e00
Max Kolysh <maxko87@gmail.com>
2013-04-08 02:25:54 -0400

    can exit and reenter focus mode

--- mockup/js/index.js
+++ mockup/js/index.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
     var words_per_modal = 25;
+    var index = 0;
+    var words = [];
 
     //actually a really dumb split for now
     var smartSplit = function(data) {
         words = [
-            "Elena first met Hernán at a bar. She was in her early twenties, hanging out in a Juárez club frequented by people involved in the drug world, people who partied hard and were always flush with cash.",
-            "Elena spotted Hernán across the room and asked a friend to introduce them. She was aggressive that way. She was also strikingly attractive and had a wild streak that made her uninterested in stable men with stable careers.",
+            "Elena first met Hernan at a bar. She was in her early twenties, hanging out in a Juarez club frequented by people involved in the drug world, people who partied hard and were always flush with cash.",
+            "Elena spotted Hernan across the room and asked a friend to introduce them. She was aggressive that way. She was also strikingly attractive and had a wild streak that made her uninterested in stable men with stable careers.",
             "Elena and Hernán (all the names in this piece are pseudonyms) soon became a couple, of sorts—he already had a wife and children, and other mistresses. But Elena was different than the docile women he was accustomed to.",
             "If he pushed her she pushed back. She was not afraid of his violent character—her father was abusive, as were many of the men she’d been with since her adolescence, when she’d discovered her sexuality.",
         ];
@@ -15,8 +17,8 @@ $(document).ready(function() {
   
     //data contains the text to be focused and modal'd
     var startFocus = function(data) {
-        words = smartSplit(data); //array of paragraphs per modal
-        index = 0;
+        if (words.length == 0)
+            words = smartSplit(data); //array of paragraphs per modal, done only the first time focus'd on that page
         $('#myModal').modal();
         $('#modalContent').text(words[index]);
     }
